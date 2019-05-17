862609eb103cae3f0eda7bf3b3e234a379d78c36
Masataka Mori <masataka.mori20@gmail.com>
2019-05-17 11:26:04 -0700

    General Graph file (template) added

--- merchant-insight-react/src/generalGraphs/GeneralGraph.js
+++ merchant-insight-react/src/generalGraphs/GeneralGraph.js
@@ -1,56 +1,67 @@
 import React from 'react';
 import '../styles/GeneralGraph.css'
-
 import {
-    PieChart, Pie, Sector, Cell,
-  } from 'recharts';
-  
-  const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-  ];
-  
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-  
-  const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({
-    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-  }) => {
-     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
-    return (
-      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
-  };
-  
-  export class GeneralGraph extends React.Component {
-    //static jsfiddleUrl = 'https://jsfiddle.net/alidingling/c9pL8k61/';
-  
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Label,
+} from 'recharts';
+import axios from 'axios';
+import { BACKEND_API, ETHNICITY, ETHNICITY_SHORT } from '../res/Constants';
+
+
+//TODO: Unique ethnicity is not fully determined yet. Relevant constants are stored in src/res/Constants.js
+export class GeneralGraph extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: undefined,
+        }
+    }
+
+    componentDidMount() {
+        const urls = ETHNICITY.map( e => BACKEND_API+'/data/ethnicity?ethnicity='+e)
+        const promises = urls.map( url => axios.get(url))
+        let result = []
+        axios.all(promises)
+            .then((responses) => {
+                for (let i = 0; i < ETHNICITY.length; i++) {
+                    result.push({
+                        ethnicity: ETHNICITY_SHORT[i],
+                        result: responses[i].data.averageOverallSatisfaction,
+                    })
+                }
+                this.setState({data: result})
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
     render() {
       return (
-        <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx={200}
-            cy={200}
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
+          <div>
             {
-              data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                this.state.data === undefined || this.state.data.length == 0
+                    ? <p>Loading</p>
+                    :
+                    <BarChart
+                    width={1200}
+                    height={500}
+                    data={this.state.data}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="ethnicity">
+                    <Label className="label" value="Average overall satisfaction by ethnicities" offset={0} position="insideBottom" />
+                    </XAxis>
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="result" fill="#8884d8" />
+                </BarChart>
             }
-          </Pie>
-        </PieChart>
+        </div>
       );
     }
-  }
+}
   
