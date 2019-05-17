3e5086f28f18844887bc70c512c206db34cc35b2
Yijing Zhou <yijingz1227@gmail.com>
2019-05-17 00:11:28 -0700

    A naive fix

--- merchant-insight-react/src/generalGraphs/GeneralGraph.js
+++ merchant-insight-react/src/generalGraphs/GeneralGraph.js
@@ -43,60 +43,84 @@ const ETHNICITY = [
   'Prefer not to answer',
 ];
 
-  
-  export class GeneralGraph extends React.Component {
-    //static jsfiddleUrl = 'https://jsfiddle.net/alidingling/c9pL8k61/';
+export class GeneralGraph extends React.Component {
+//static jsfiddleUrl = 'https://jsfiddle.net/alidingling/c9pL8k61/';
 
-    
-    // class function
-    // Send GET request based on "ethnicity"
-    // Store reponse data
-    getOverallSatifaction () {
-      var overallStaisfactionArray = [];
-      
-      for (const eth of ETHNICITY) {
+
+// class function
+// Send GET request based on "ethnicity"
+// Store reponse data
+    constructor() {
+
+        super();
+        this.state = {
+            overallStatisfaction: undefined,
+        }
+    }
+
+    componentDidMount() {
+        this.getOverallSatifaction(this.triggerRerender)
+    }
+
+    triggerRerender = (data) => {
+        this.setState({overallSatisfaction: data})
+    }
+
+    getOverallSatifaction (callback) {
+        var overallStaisfactionArray = [];
+
+        for (const eth of ETHNICITY) {
         axios.get(BACKEND_API+'/data/ethnicity?ethnicity=' + eth)
           .then(function (response) {
             //console.log(response);
             // should have avoided data manipulation at frontent
             var value = parseFloat(response.data['averageOverallSatisfaction']);
-            console.log(value);
+            //console.log(value);
             overallStaisfactionArray.push(
               {
                 ethnicity: String(eth),
                 result: value,
               }
             );
+            if (overallStaisfactionArray.length === ETHNICITY.length) {
+                callback(overallStaisfactionArray);
+            }
           })
           .catch(function (error) {
             console.log(error);
           });
       }
-      console.log(overallStaisfactionArray);
       return overallStaisfactionArray;
     }
-  
+
     render() {
+        if (this.state.overallSatisfaction !== undefined) {
+            console.log(JSON.stringify(this.state.overallSatisfaction))
+        }
       return (
-        <div>
-          {this.getOverallSatifaction()}
-          <BarChart
-            width={500}
-            height={300}
-            data={this.getOverallSatifaction()}
-            margin={{
-              top: 5, right: 30, left: 20, bottom: 5,
-            }}
-          >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="ethnicity" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="result" fill="#8884d8" />
-          </BarChart>
+          <div>
+            {
+                this.state.overallSatisfaction === undefined || this.state.overallSatisfaction.length == 0
+                    ? <p>Loading</p>
+                    :
+                    <BarChart
+                    width={2000}
+                    height={300}
+                    data={this.state.overallSatisfaction}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="ethnicity" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="result" fill="#8884d8" />
+                </BarChart>
+            }
         </div>
       );
     }
-  }
+}
   
