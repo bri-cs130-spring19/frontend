53379e7e34244bcf3f0dfd732fda7c64580c5cf4
Daniel Achee <danielachee@Daniels-MacBook-Air.local>
2019-06-07 12:29:20 -0700

    Made DeviceType Mobile Compat

--- merchant-insight-react/src/generalGraphs/DeviceType.js
+++ merchant-insight-react/src/generalGraphs/DeviceType.js
@@ -5,17 +5,25 @@ import {
 } from 'recharts';
 import axios from 'axios';
 import { BACKEND_API, DEVICE_TYPE } from '../res/Constants';
+import { Assistant } from "../components/Assistant";
 
 export class DeviceType extends React.Component {
     constructor() {
         super();
         this.state = {
             data: undefined,
+            collapsed: false,
+            isBigScreen: true,
+            visible: false,
+            top: 12,
         }
         console.log("device type class constructor")
     }
 
-    componentDidMount() {        
+    componentDidMount() {
+        this.siderOrDrawer();
+        window.addEventListener("resize", this.siderOrDrawer.bind(this));
+
         const urls = DEVICE_TYPE.map( e => BACKEND_API+'/data/device_type?device_type='+e)
         const promises = urls.map( url => axios.get(url))
         let result = []
@@ -35,15 +43,25 @@ export class DeviceType extends React.Component {
 
     }
 
+    siderOrDrawer() {
+        if (window.innerWidth < 992) {
+            this.setState({isBigScreen: false});
+        } else {
+            this.setState({isBigScreen: true});
+        }
+    }
+
     render() {
       return (
+          this.state.isBigScreen?
           <div>
             {
                 this.state.data === undefined || this.state.data.length == 0
                     ? <p>Loading</p>
                     :
+                    <div>
                     <BarChart
-                    width={1000}
+                    width={800}
                     height={500}
                     data={this.state.data}
                     margin={{
@@ -58,6 +76,39 @@ export class DeviceType extends React.Component {
                     <Tooltip />
                     <Bar dataKey="result" fill="#8884d8" />
                 </BarChart>
+                <Assistant
+                text={"Users are least satisfied when using an iPad."}
+                /> 
+                </div>
+            }
+        </div>
+        :
+        <div>
+            {
+                this.state.data === undefined || this.state.data.length == 0
+                    ? <p>Loading</p>
+                    :
+                    <div>
+                    <BarChart
+                    width={window.innerWidth - 100}
+                    height={250}
+                    data={this.state.data}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="device_type">
+                    <Label className="label" value="Average overall satisfaction by device type" offset={0} position="insideBottom" />
+                    </XAxis>
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="result" fill="#8884d8" />
+                </BarChart>
+                <Assistant
+                text={"Users are least satisfied when using an iPad."}
+                /> 
+                </div>
             }
         </div>
       );
