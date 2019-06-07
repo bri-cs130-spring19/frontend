5a6c00a88861057c343b3d1b50c2d770fdd199d8
Yijing Zhou <yijingz1227@gmail.com>
2019-06-07 00:58:21 -0700

    Readjusted the dashboard for mobile view. Integrated with the backend API calls.

--- merchant-insight-react/src/dashboard/Sidebar.js
+++ merchant-insight-react/src/dashboard/Sidebar.js
@@ -13,6 +13,10 @@ import {
   Legend
 } from "recharts";
 import PropTypes from "prop-types";
+import {BACKEND_API, US_STATES, US_STATES_STATE_CODES} from "../res/Constants";
+import axios from "axios";
+import {Col, Row} from "react-bootstrap";
+import {Spin} from "antd";
 
 const fakeGenderData = [
   { name: "female", value: 600 },
@@ -20,12 +24,12 @@ const fakeGenderData = [
 ];
 
 const fakeSatisfactionTrend = [
-  { name: "Jan", satisfaction: 9800, amt: 9.2 },
-  { name: "Feb", satisfaction: 9800, amt: 9.2 },
-  { name: "Mar", satisfaction: 9800, amt: 9.0 },
-  { name: "Apr", satisfaction: 3908, amt: 8.7 },
-  { name: "May", satisfaction: 4800, amt: 8.8 },
-  { name: "June", satisfaction: 4800, amt: 8.5 }
+  { name: "Jan", satisfaction: 9.2, amt: 9.2 },
+  { name: "Feb", satisfaction: 9.2, amt: 9.2 },
+  { name: "Mar", satisfaction: 9.0, amt: 9.0 },
+  { name: "Apr", satisfaction: 8.7, amt: 8.7 },
+  { name: "May", satisfaction: 8.8, amt: 8.8 },
+  { name: "June", satisfaction: 8.5, amt: 8.5 }
 ];
 
 const COLORS = ["#0088FE", "#FF8042"];
@@ -39,7 +43,6 @@ const renderCustomizedLabel = ({
   innerRadius,
   outerRadius,
   percent,
-  index
 }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
@@ -59,63 +62,108 @@ const renderCustomizedLabel = ({
 };
 
 export class Sidebar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isSatiafaction: false,
+            data: undefined,
+        };
+    }
+
+    componentDidMount() {
+        const urls = US_STATES_STATE_CODES.map( e => BACKEND_API+'/data/states/'+e);
+        const promises = urls.map( url => axios.get(url));
+        let result = [];
+        axios.all(promises)
+            .then((responses) => {
+                for (let i = 0; i < US_STATES.length; i++) {
+                    var female = {};
+                    var male = {};
+                    female.name = "female";
+                    male.name = "male";
+                    female.value = responses[i].data.numberFemales;
+                    male.value = responses[i].data.numberMales;
+                    result.push([female, male]);
+
+                }
+                this.setState({data: result});
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
   static propTypes = {
     curState: PropTypes.object.isRequired
   };
 
   render() {
-    return (
-      <div>
-        <Assistant
-          text={"Your statistics for " + this.props.curState.id + " is here."}
-        />
-        <Assistant
-          text={
-            "For the past 6 months, consumer from " +
-            this.props.curState.id +
-            " has the following overall satisfaction."
-          }
-        />
-        <LineChart
-          width={300}
-          height={200}
-          data={fakeSatisfactionTrend}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="satisfaction"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-        <Assistant text={"The gender distribution is :"} />
-        <PieChart width={300} height={200}>
-          <Pie
-            data={fakeGenderData}
-            cx={200}
-            cy={100}
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {fakeGenderData.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-          <Tooltip />
-        </PieChart>
-      </div>
-    );
+
+      if (this.state.data === undefined || this.state.data.length === 0) {
+                return ( <div>
+                    <Row className="loading">
+                        <Col lg={12} md={12} sm={12} xs={12}>
+                            <Spin />
+                        </Col>
+                    </Row>
+                </div>)
+      } else {
+          return(
+              <div>
+                  <Assistant
+                      text={"Your statistics for " + this.props.curState.id + " is here."}
+                  />
+                  <Assistant
+                      text={
+                          "For the past 6 months, consumer from " +
+                          this.props.curState.id +
+                          " has the following overall satisfaction."
+                      }
+                  />
+                  <LineChart
+                      width={300}
+                      height={200}
+                      data={fakeSatisfactionTrend}
+                      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                  >
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <Tooltip />
+                      <Legend />
+                      <Line
+                          type="monotone"
+                          dataKey="satisfaction"
+                          stroke="#8884d8"
+                          activeDot={{ r: 8 }}
+                      />
+                  </LineChart>
+                  <Assistant text={"The gender distribution is :"} />
+                  <PieChart width={300} height={200}>
+                      <Pie
+                          data={this.state.data[US_STATES.indexOf(this.props.curState.id)]}
+                          cx={150}
+                          cy={100}
+                          labelLine={false}
+                          label={renderCustomizedLabel}
+                          outerRadius={80}
+                          fill="#8884d8"
+                          dataKey="value"
+                      >
+                          {fakeGenderData.map((entry, index) => (
+                              <Cell
+                                  key={`cell-${index}`}
+                                  fill={COLORS[index % COLORS.length]}
+                              />
+                          ))}
+                      </Pie>
+                      <Tooltip />
+                  </PieChart>
+              </div>
+          )
+      }
+
+
   }
 }
