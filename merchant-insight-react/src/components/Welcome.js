23b6685e88eb5686eff4be128d0cc8737abbbb7b
NinaMaller <nina.maller@gmail.com>
2019-06-07 12:26:49 -0700

    welcome page and other

--- merchant-insight-react/src/components/Welcome.js
+++ merchant-insight-react/src/components/Welcome.js
@@ -1,7 +1,23 @@
 import React from "react";
+import '../styles/Welcome.css'
 
 export class Welcome extends React.Component {
   render() {
-    return <div>This is Welcome Page. A temporary placeholder.</div>;
+    return (
+    
+    
+      <div className="landing-body">
+      <div className="landing-wrapper">
+        <div className="landing-header">
+          Welcome.   
+        </div>
+        <div className="landing-body-font">
+            We are the merchant insight team. Thanks for taking the time to look at our demo :)
+        </div>
+      </div>
+    </div>
+
+    );
+
   }
 }
